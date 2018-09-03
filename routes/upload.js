3869b749d3deeba23443be43330f5e3e8231a06b
Johnson-Han <hanlifeng@sinap.ac.cn>
2018-09-03 11:56:32 +0800

    上传语句

--- routes/upload.js
+++ routes/upload.js
@@ -35,23 +35,7 @@ router.post('/alloy', multer({ storage: storage3 }).single('file'), function (re
              console.log(result.rows);
          });
      }
-    //     newDate.setTime(upfdate + localOffset);
-    //     var filepath = "/files/" + req.file.filename;
-    //     sql = 'insert into cer_files (cer_index,origin_name,filename,file_addr,upload_date) values (\'{' + req.body.id + '}\',\'' + req.file.originalname + '\',\'' + req.file.filename + '\',\'' + filepath + '\',\'' + upftime + '\')';
-    //     console.log(sql);
-    //     pg2.query(sql, function (result) { });
-    // }
-    // var tmppath = req.files.file.path;
-    // fs.createReadStream(__dirname + '/' + tmppath)
-    //     .pipe(parse({ delimiter: "\n" }))
-    //     .on('data', function (csvrow) {
-    //         console.log(csvrow);
-    //         csvData.push(csvrow);
-    //     })
-    //     .on('end', function () {
-    //         console.log(csvData);
-    //     });
- 
+
 
 
 
