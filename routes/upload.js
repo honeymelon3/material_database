639117e9278db5e8c9e95ed8ba1f700221eaa134
Johnson-Han <hanlifeng@sinap.ac.cn>
2018-09-04 09:22:31 +0800

    encoding

--- routes/upload.js
+++ routes/upload.js
@@ -48,7 +48,7 @@ router.post('/alloy', multer({ storage: storage3 }).single('file'), function (re
                  my_conn.query(sql, function (result) {
                      console.log(result.rows);
                      Wurl = '/data_alloy';
-                     res.redirect(Wurl);
+                    //  res.redirect(Wurl);
                  });                 
              });
 
