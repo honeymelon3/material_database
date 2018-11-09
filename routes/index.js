33645ca802d28d5dc5d2dc67b8764f5f2647c144
Johnson Han <hanlifeng@sinap.ac.cn>
2018-11-09 20:02:01 +0800

    测试

--- routes/index.js
+++ routes/index.js
@@ -578,7 +578,7 @@ router.get('/salt_list50/:param_id', function(req, res,next) {
  router.get('/corrode_list50/:alloy_name', function(req, res,next) { 
    	//var sql = 'select * from db_user where username = \'jyq\'' ;
 	//var sql = 'SELECT column_name from information_schema.columns where table_name = \'alloy_param_data\'' ;
-	 var sql = 'select * from alloy_salt_corrod left join salt_element on alloy_salt_corrod.salt_batch=salt_element.salt_batch' + ' where alloy_name =\'' + req.params.alloy_name.replce(/%20/g, '&#32;')+ '\';';
+	 var sql = 'select * from alloy_salt_corrod left join salt_element on alloy_salt_corrod.salt_batch=salt_element.salt_batch' + ' where alloy_name =\'' + req.params.alloy_name + '\';';
 	console.log(sql);
 	 my_conn.query(sql,function(result){		
 		res.jsonp(result.rows);
