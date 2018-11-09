4bdaaefc0a2300de42231bd99870ae1a5930c719
Johnson Han <hanlifeng@sinap.ac.cn>
2018-11-10 00:47:28 +0800

    增加类型

--- routes/upload.js
+++ routes/upload.js
@@ -43,7 +43,7 @@ router.post('/alloy', multer({ storage: storage3 }).single('file'), function (re
              console.log(sql);
              my_conn.query(sql, function (result) {
                  console.log(result.rows);
-                 var sql = 'copy alloy_param_data(alloy_grade,alloy_batch,test_temp,effect_factor1_value,effect_factor1_name,effect_factor2_value,effect_factor2_name,standard_id,param_id,report_id,test_stress,alloy_shape,material_source,material_name,test_time,test_duration,test_direction,test_org,effect_factor3_value,effect_factor3_name,db_type,note,"alloy_spec.No","alloy_type/grade",effect_factor4_value,effect_factor4_name,fracture_profile,creep_curve,metallograph,param_value,effect_factor_name,effect_factor_value) from \'' + req.file.path + '\' with delimiter as \',\' csv header;';
+                 var sql = 'copy alloy_param_data(alloy_grade,alloy_batch,test_temp,effect_factor1_value,effect_factor1_name,effect_factor2_value,effect_factor2_name,standard_id,param_id,report_id,test_stress,alloy_shape,material_source,material_name,test_time,test_duration,test_direction,test_org,effect_factor3_value,effect_factor3_name,db_type,note,"alloy_spec.No","alloy_type/grade",effect_factor4_value,effect_factor4_name,fracture_profile,creep_curve,metallograph,param_value,effect_factor_name,effect_factor_value,data_category) from \'' + req.file.path + '\' with delimiter as \',\' csv header;';
                  console.log(sql);
                  my_conn.query(sql, function (result) {
                      console.log(result.rows);
@@ -87,7 +87,7 @@ router.post('/graphite', multer({ storage: storage3 }).single('file'), function 
             console.log(sql);
             my_conn.query(sql, function (result) {
                 console.log(result.rows);
-                var sql = 'copy graphite_param_data("material_grade","material_batch","standard_id","param_value","param_id","report_id","material_block_size","data_source","test_time","test_infiltration_duration","test_org","note","material_manufacturer","material_block_num","material_produce_date","sample_direction","sample_num_avg","sample_num","molten_salt_composition_id","molten_salt_batch","inflitration_experiment_no","test_preset_pres","test_inflitration_pres","test_inflitration_temp","molten_salt_content","test_notch_depth","test_support_span","molten_salt_distribution","db_type","sample_org","sample_index","sample_position_id","maintainer_id","test_temp","test_temp_span","sample_size") from \'' + req.file.path + '\' with delimiter as \',\' csv header;';
+                var sql = 'copy graphite_param_data("material_grade","material_batch","standard_id","param_value","param_id","report_id","material_block_size","data_source","test_time","test_infiltration_duration","test_org","note","material_manufacturer","material_block_num","material_produce_date","sample_direction","sample_num_avg","sample_num","molten_salt_composition_id","molten_salt_batch","inflitration_experiment_no","test_preset_pres","test_inflitration_pres","test_inflitration_temp","molten_salt_content","test_notch_depth","test_support_span","molten_salt_distribution","db_type","sample_org","sample_index","sample_position_id","maintainer_id","test_temp","test_temp_span","sample_size","data_category") from \'' + req.file.path + '\' with delimiter as \',\' csv header;';
                 console.log(sql);
                 my_conn.query(sql, function (result) {
                     console.log(result.rows);
@@ -131,7 +131,7 @@ router.post('/salt', multer({ storage: storage3 }).single('file'), function (req
             console.log(sql);
             my_conn.query(sql, function (result) {
                 console.log(result.rows);
-                var sql = 'copy salt_param_data(comp_purity,ele_impurity,salt_batch,salt_state,salt_pretreatment,salt_test_temp,salt_test_temp_range,param_id,param_value,param_uncertainty,param_value_fitting,param_method,param_source,param_check,note,param_report,salt_id) from \'' + req.file.path + '\' with delimiter as \',\' csv header;';
+                var sql = 'copy salt_param_data(comp_purity,ele_impurity,salt_batch,salt_state,salt_pretreatment,salt_test_temp,salt_test_temp_range,param_id,param_value,param_uncertainty,param_value_fitting,param_method,param_source,param_check,note,param_report,salt_id,data_category) from \'' + req.file.path + '\' with delimiter as \',\' csv header;';
                 console.log(sql);
                 my_conn.query(sql, function (result) {
                     console.log(result.rows);
@@ -174,7 +174,7 @@ router.post('/irradiation', multer({ storage: storage3 }).single('file'), functi
             console.log(sql);
             my_conn.query(sql, function (result) {
                 console.log(result.rows);
-                var sql = 'copy alloy_irradiant_data("sample_id","alloy_name","alloy_shape","sample_direction","annealing_temp","irradiation_reactor","enviroment","temperature_lower_limit ","temperature_upper_limit","ave_temperature","time_at_temperature","neutron_dose_1","neutron_dose_2","neutron_dose_3","neutron_dose_4","neutron_dose_5","neutron_dose_6","DPA","He","test_time","test_temp","test_env","strain_rate","test_stress","param_id","param_value","source","test_standard","report_name","alloy_batch","annealing_time","neutron_dose") from \'' + req.file.path + '\' with delimiter as \',\' csv header;';
+                var sql = 'copy alloy_irradiant_data("sample_id","alloy_name","alloy_shape","sample_direction","annealing_temp","irradiation_reactor","enviroment","temperature_lower_limit ","temperature_upper_limit","ave_temperature","time_at_temperature","neutron_dose_1","neutron_dose_2","neutron_dose_3","neutron_dose_4","neutron_dose_5","neutron_dose_6","DPA","He","test_time","test_temp","test_env","strain_rate","test_stress","param_id","param_value","source","test_standard","report_name","alloy_batch","annealing_time","neutron_dose","data_category") from \'' + req.file.path + '\' with delimiter as \',\' csv header;';
                 console.log(sql);
                 my_conn.query(sql, function (result) {
                     console.log(result.rows);
@@ -217,7 +217,7 @@ router.post('/corrode', multer({ storage: storage3 }).single('file'), function (
             console.log(sql);
             my_conn.query(sql, function (result) {
                 console.log(result.rows);
-                var sql = 'copy alloy_salt_corrod("alloy_name","alloy_state","alloy_batch","salt_batch","salt_batch_add","methods","test_temp","duration","environment","crucible","setup","fixing","weight_loss","uncertainty","morphology","standard_name","param_maintainer","param_check","note","salt_name_id","report_name","depth_in_base","depth_in_weld","dt","flow_rate","references","corrdepth_morphology(um)","corrodepth_Cr(um)","corrdepth_weight(um)") from \'' + req.file.path + '\' with delimiter as \',\' csv header;';
+                var sql = 'copy alloy_salt_corrod("alloy_name","alloy_state","alloy_batch","salt_batch","salt_batch_add","methods","test_temp","duration","environment","crucible","setup","fixing","weight_loss","uncertainty","morphology","standard_name","param_maintainer","param_check","note","salt_name_id","report_name","depth_in_base","depth_in_weld","dt","flow_rate","references","corrdepth_morphology(um)","corrodepth_Cr(um)","corrdepth_weight(um)","data_category") from \'' + req.file.path + '\' with delimiter as \',\' csv header;';
                 console.log(sql);
                 my_conn.query(sql, function (result) {
                     console.log(result.rows);
