8bd7d4b2333d6940af2e63cfbd11cfcc5bd71cc6
Johnson Han <hanlifeng@sinap.ac.cn>
2018-12-07 11:49:41 +0800

    asdfa

--- routes/index.js
+++ routes/index.js
@@ -473,7 +473,7 @@ router.get('/data_corrode/:alloy_name', function (req, res, next) {
 		//  res.jsonp(result.rows);
 		 ////console.log(result.rows);
 		//  console.log(result.rows); 
-		 var sql = 'select ' + result.rows[0].param_scope +' from alloy_param_data,alloy_shape,test_direction,alloy_param, alloy_standard_info,alloy_report_info,alloy_batch,alloy_test_org where alloy_param_data.param_id=alloy_param.id and  alloy_param_data.standard_id =alloy_standard_info.id and alloy_param_data.report_id =alloy_report_info.id and alloy_param_data.alloy_batch_id =alloy_batch.id and alloy_param_data.alloy_shape_id =alloy_shape.id and alloy_param_data.test_direction_id =test_direction.id and alloy_param_data.test_org_id =alloy_test_org.id and alloy_param_data.material_name_id =alloy_name.id and param_id=\'' + req.params.param_id + '\'';
+		 var sql = 'select ' + result.rows[0].param_scope +' from alloy_param_data,alloy_shape,alloy_name,test_direction,alloy_param, alloy_standard_info,alloy_report_info,alloy_batch,alloy_test_org where alloy_param_data.param_id=alloy_param.id and  alloy_param_data.standard_id =alloy_standard_info.id and alloy_param_data.report_id =alloy_report_info.id and alloy_param_data.alloy_batch_id =alloy_batch.id and alloy_param_data.alloy_shape_id =alloy_shape.id and alloy_param_data.test_direction_id =test_direction.id and alloy_param_data.test_org_id =alloy_test_org.id and alloy_param_data.material_name_id =alloy_name.id and param_id=\'' + req.params.param_id + '\'';
 		 console.log(sql);
 		 my_conn.query(sql, function (result) {
 			 res.jsonp(result.rows);
